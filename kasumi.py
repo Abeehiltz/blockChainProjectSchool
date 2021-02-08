cf195fa1083d12c720783de56d8f784f9851ea0e
elizabeth BRICOUT <elizabeth.bricout@utt.fr>
2021-02-08 19:20:18 +0100

    Kasumi fini, juste un leger truc à la fin du fichier lorsqu'on décode.

--- kasumi.py
+++ kasumi.py
@@ -1,10 +1,11 @@
 # decalage vers la gauche de s bits avec les s bits de gauche reviennent à droite
-import re
 import sys
+import pyfinite.ffield
 
+VECTEUR_INIT = b"ABCDEFGH"
 
-def _circular_shift(x, s):
-    return ((x << s) & 0xFFFF) | (x >> (16 - s))
+def _circular_shift(x, s, nbBits = 16):
+    return ((x << s) & 0xFFFF) | (x >> (nbBits - s))
 
 
 def _mod(x):
@@ -15,55 +16,8 @@ def bytes_xor(a, b) :
     return bytes(x ^ y for x, y in zip(a, b))
 
 
-S7 = (
-    54, 50, 62, 56, 22, 34, 94, 96, 38, 6, 63, 93, 2, 18, 123, 33,
-    55, 113, 39, 114, 21, 67, 65, 12, 47, 73, 46, 27, 25, 111, 124, 81,
-    53, 9, 121, 79, 52, 60, 58, 48, 101, 127, 40, 120, 104, 70, 71, 43,
-    20, 122, 72, 61, 23, 109, 13, 100, 77, 1, 16, 7, 82, 10, 105, 98,
-    117, 116, 76, 11, 89, 106, 0, 125, 118, 99, 86, 69, 30, 57, 126, 87,
-    112, 51, 17, 5, 95, 14, 90, 84, 91, 8, 35, 103, 32, 97, 28, 66,
-    102, 31, 26, 45, 75, 4, 85, 92, 37, 74, 80, 49, 68, 29, 115, 44,
-    64, 107, 108, 24, 110, 83, 36, 78, 42, 19, 15, 41, 88, 119, 59, 3,
-)
-
-S9 = (
-    167, 239, 161, 379, 391, 334, 9, 338, 38, 226, 48, 358, 452, 385, 90, 397,
-    183, 253, 147, 331, 415, 340, 51, 362, 306, 500, 262, 82, 216, 159, 356, 177,
-    175, 241, 489, 37, 206, 17, 0, 333, 44, 254, 378, 58, 143, 220, 81, 400,
-    95, 3, 315, 245, 54, 235, 218, 405, 472, 264, 172, 494, 371, 290, 399, 76,
-    165, 197, 395, 121, 257, 480, 423, 212, 240, 28, 462, 176, 406, 507, 288, 223,
-    501, 407, 249, 265, 89, 186, 221, 428, 164, 74, 440, 196, 458, 421, 350, 163,
-    232, 158, 134, 354, 13, 250, 491, 142, 191, 69, 193, 425, 152, 227, 366, 135,
-    344, 300, 276, 242, 437, 320, 113, 278, 11, 243, 87, 317, 36, 93, 496, 27,
-    487, 446, 482, 41, 68, 156, 457, 131, 326, 403, 339, 20, 39, 115, 442, 124,
-    475, 384, 508, 53, 112, 170, 479, 151, 126, 169, 73, 268, 279, 321, 168, 364,
-    363, 292, 46, 499, 393, 327, 324, 24, 456, 267, 157, 460, 488, 426, 309, 229,
-    439, 506, 208, 271, 349, 401, 434, 236, 16, 209, 359, 52, 56, 120, 199, 277,
-    465, 416, 252, 287, 246, 6, 83, 305, 420, 345, 153, 502, 65, 61, 244, 282,
-    173, 222, 418, 67, 386, 368, 261, 101, 476, 291, 195, 430, 49, 79, 166, 330,
-    280, 383, 373, 128, 382, 408, 155, 495, 367, 388, 274, 107, 459, 417, 62, 454,
-    132, 225, 203, 316, 234, 14, 301, 91, 503, 286, 424, 211, 347, 307, 140, 374,
-    35, 103, 125, 427, 19, 214, 453, 146, 498, 314, 444, 230, 256, 329, 198, 285,
-    50, 116, 78, 410, 10, 205, 510, 171, 231, 45, 139, 467, 29, 86, 505, 32,
-    72, 26, 342, 150, 313, 490, 431, 238, 411, 325, 149, 473, 40, 119, 174, 355,
-    185, 233, 389, 71, 448, 273, 372, 55, 110, 178, 322, 12, 469, 392, 369, 190,
-    1, 109, 375, 137, 181, 88, 75, 308, 260, 484, 98, 272, 370, 275, 412, 111,
-    336, 318, 4, 504, 492, 259, 304, 77, 337, 435, 21, 357, 303, 332, 483, 18,
-    47, 85, 25, 497, 474, 289, 100, 269, 296, 478, 270, 106, 31, 104, 433, 84,
-    414, 486, 394, 96, 99, 154, 511, 148, 413, 361, 409, 255, 162, 215, 302, 201,
-    266, 351, 343, 144, 441, 365, 108, 298, 251, 34, 182, 509, 138, 210, 335, 133,
-    311, 352, 328, 141, 396, 346, 123, 319, 450, 281, 429, 228, 443, 481, 92, 404,
-    485, 422, 248, 297, 23, 213, 130, 466, 22, 217, 283, 70, 294, 360, 419, 127,
-    312, 377, 7, 468, 194, 2, 117, 295, 463, 258, 224, 447, 247, 187, 80, 398,
-    284, 353, 105, 390, 299, 471, 470, 184, 57, 200, 348, 63, 204, 188, 33, 451,
-    97, 30, 310, 219, 94, 160, 129, 493, 64, 179, 263, 102, 189, 207, 114, 402,
-    438, 477, 387, 122, 192, 42, 381, 5, 145, 118, 180, 449, 293, 323, 136, 380,
-    43, 66, 60, 455, 341, 445, 202, 432, 8, 237, 15, 376, 436, 464, 59, 461,
-)
-
-
 class Kasumi:
-    def __init__(self):
+    def __init__(self, master_key):
         self.key_KL1 = [None] * 9
         self.key_KL2 = [None] * 9
         self.key_KO1 = [None] * 9
@@ -72,9 +26,9 @@ class Kasumi:
         self.key_KI1 = [None] * 9
         self.key_KI2 = [None] * 9
         self.key_KI3 = [None] * 9
-        self.S = [None] * 255
 
-    def set_key(self, master_key):
+        self.corps_galois = pyfinite.ffield.FField(16)
+
         key = [None] * 9
         key_prime = [None] * 9
 
@@ -95,34 +49,45 @@ class Kasumi:
             self.key_KI2[i] = key_prime[_mod(i + 3)]
             self.key_KI3[i] = key_prime[_mod(i + 7)]
 
-        for i in range(0, 255):
-            self.S[i] = i
 
-        for i in range(0, 255):
-            j = (j + self.S[i] + master_key[i % len(master_key)]) % 256
-            self.S[i], self.S[j] = self.S[j], self.S[i]
 
+
+        # init RC4
+        self.s_box_1 = [i for i in range(0,256)]
+        self.s_box_2 = [i for i in range(0,256)]
+
+        a = 0
+        b = 0
+
+        k_left = master_key >> 64
+        k_right = master_key & 0xFFFFFFFFFFFFFFFFFFFFFFFF
+        k_left = k_left.to_bytes(64, sys.byteorder)
+        k_right = k_right.to_bytes(64, sys.byteorder)
+
+        for i in range (0,256):
+            a = (a + self.s_box_1[i]+k_left[i%64])%256
+            b = (b + self.s_box_2[i]+k_right[i%64])%256
+
+            self.s_box_1[i], self.s_box_1[a] = self.s_box_1[a], self.s_box_1[i]
+            self.s_box_2[i], self.s_box_2[b] = self.s_box_2[b], self.s_box_2[i]
 
 
     def FI_fun(self, round_key_KI, input):
 
-        left_0 = input >> 7
-        right_0 = input & 0b1111111
+        round_key_KI = _circular_shift(round_key_KI, 2)
 
-        # separating in two parts so that it won't change the size during the xor in right and left 2
-        round_key_1 = round_key_KI >> 9
-        round_key_2 = round_key_KI & 0b111111111
+        #divide input by two
+        left_0 = input >> 8
+        right_0 = input & 0b11111111
 
-        right_1 = S9[left_0] ^ right_0
-        left_1 = S7[right_0] ^ (right_1 & 0b1111111)
 
-        right_2 = right_1 ^ round_key_2
-        left_2 = left_1 ^ round_key_1
+        left = self.s_box_1[left_0]
+        right = self.s_box_2[right_0]
 
-        right_3 = S9[right_2] ^ left_2
-        left_3 = S7[left_2] ^ (right_3 & 0b1111111)
 
-        return (left_3 << 9) | right_3
+        return round_key_KI ^ (left+right)
+
+
 
     def FO_fun(self, input, round_i):
         input_l = input >> 16
@@ -140,11 +105,15 @@ class Kasumi:
         return (left_3 << 16) | right_3
 
     def FL_fun(self, input, round_i):
+
         input_l = input >> 16
         input_r = input & 0xFFFF
 
         right_prime = _circular_shift(input_l & self.key_KL1[round_i], 1) ^ input_r
         left_prime = _circular_shift(right_prime | self.key_KL2[round_i], 1) ^ input_l
+
+        right_prime = self.corps_galois.Inverse(right_prime)
+        left_prime = self.corps_galois.Inverse(left_prime)
 
         return (left_prime << 16) | right_prime
 
@@ -187,8 +156,8 @@ class Kasumi:
 
 
 def chiffrement_ECB(kasumi, message):
-    messageChiffre = "".encode()
-    message = message.encode()
+    messageChiffre = "".encode('utf-8')
+    message = message.encode('utf-8')
 
     # Separate message en partie de 64 bits (8 octets)
     parts = [message[i:i + 8] for i in range(0, len(message), 8)]
@@ -200,7 +169,7 @@ def chiffrement_ECB(kasumi, message):
 
 
 def dechiffrement_ECB(kasumi, message):
-    messageDechiffre = "".encode()
+    messageDechiffre = "".encode('utf-8')
 
     # Separate message en partie de 64 bits (8 octets)
     parts = [message[i:i + 8] for i in range(0, len(message), 8)]
@@ -208,15 +177,15 @@ def dechiffrement_ECB(kasumi, message):
     for part in parts:
         messageDechiffre += kasumi.decoding(int.from_bytes(part, sys.byteorder)).to_bytes(8, sys.byteorder)
 
-    return messageDechiffre.decode()
+    messageDechiffre = messageDechiffre.rstrip(b'\x00')
+    return messageDechiffre.decode('utf-8')
 
 
 def chiffrement_CBC(kasumi, message):
-    vecteurInit = 11709375263224690697
-    vecteurInit = vecteurInit.to_bytes(8, sys.byteorder)
+    vecteurInit = VECTEUR_INIT
 
-    messageChiffre = "".encode()
-    message = message.encode()
+    messageChiffre = "".encode('utf-8')
+    message = message.encode('utf-8')
 
     # Separate message en partie de 64 bits (8 octets)
     parts = [message[i:i + 8] for i in range(0, len(message), 8)]
@@ -240,10 +209,9 @@ def chiffrement_CBC(kasumi, message):
 
 
 def dechiffrement_CBC(kasumi, message):
-    vecteurInit = 11709375263224690697
-    vecteurInit = vecteurInit.to_bytes(8, sys.byteorder)
+    vecteurInit = VECTEUR_INIT
 
-    messageDechiffre = "".encode()
+    messageDechiffre = "".encode('utf-8')
 
     # Separate message en partie de 64 bits (8 octets)
     parts = [message[i:i + 8] for i in range(0, len(message), 8)]
@@ -264,15 +232,14 @@ def dechiffrement_CBC(kasumi, message):
 
         previousPart = part
 
-    # divise en blocks de 64
-    return messageDechiffre.decode()
+    messageDechiffre = messageDechiffre.rstrip(b'\x00')
+    return messageDechiffre.decode('utf-8')
 
 def chiffrement_PCBC(kasumi, message):
-    vecteurInit = 11709375263224690697
-    vecteurInit = vecteurInit.to_bytes(8, sys.byteorder)
+    vecteurInit = VECTEUR_INIT
 
-    messageChiffre = "".encode()
-    message = message.encode()
+    messageChiffre = "".encode('utf-8')
+    message = message.encode('utf-8')
 
     # Separate message en partie de 64 bits (8 octets)
     parts = [message[i:i + 8] for i in range(0, len(message), 8)]
@@ -281,7 +248,7 @@ def chiffrement_PCBC(kasumi, message):
 
     newVector = parts[0]
     for part in parts:
-        encode = "".encode()
+        encode = "".encode('utf-8')
         if not vecteurUsed:
             encode = bytes_xor(vecteurInit, part)
             vecteurUsed = True
@@ -296,10 +263,9 @@ def chiffrement_PCBC(kasumi, message):
     return messageChiffre
 
 def dechiffrement_PCBC(kasumi, message):
-    vecteurInit = 11709375263224690697
-    vecteurInit = vecteurInit.to_bytes(8, sys.byteorder)
+    vecteurInit = VECTEUR_INIT
 
-    messageDechiffre = "".encode()
+    messageDechiffre = "".encode('utf-8')
 
     # Separate message en partie de 64 bits (8 octets)
     parts = [message[i:i + 8] for i in range(0, len(message), 8)]
@@ -320,8 +286,8 @@ def dechiffrement_PCBC(kasumi, message):
 
         newVector = bytes_xor(decode, part)
 
-    # divise en blocks de 64
-    return messageDechiffre.decode()
+    messageDechiffre = messageDechiffre.rstrip(b'\x00')
+    return messageDechiffre.decode('utf-8')
 
 def openFile(path):
     file = open(path, 'r')
@@ -337,8 +303,7 @@ def saveFileDecrypt(path, message):
 
 if __name__ == '__main__':
     key = 0x9900aabbccddeeff1122334455667788
-    my_kasumi = Kasumi()
-    my_kasumi.set_key(key)
+    my_kasumi = Kasumi(key)
     file = openFile("message")
     text = file.read()
 
